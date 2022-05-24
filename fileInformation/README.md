946fca7036d34d3d224834131740f6339b4a8c75
Matteo <matteo.brachetta@studenti.unicam.it>
2022-05-24 07:37:55 +0000

    creazione struttura progetto

---
+++ fileInformation/README.md
@@ -0,0 +1,15 @@
+# fileInformation
+### CMake (execute file)
+comandi linux.
+
+**Posizione di partenza**`./So2022`
+
+digitare i comandi sotto elencati per eseguire il progetto:
+
+``` bash
+cd fileInfrmation
+cd build
+cmake ..
+cmake --build .
+./fielInformation
+```
---
+++ fileInformation/build/CMakeFiles/fileInformation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fileInformation.dir/main.o"
+  "fileInformation"
+  "fileInformation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/fileInformation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
