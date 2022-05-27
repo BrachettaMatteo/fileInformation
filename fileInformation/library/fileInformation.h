729e4b1966981c9394792c34ee415c8630443640
Matteo <matteo.brachetta@studenti.unicam.it>
2022-05-27 15:59:54 +0200

    implementazione nomeProprietario, numeroCaratteri, dataUltimaModifica, checkPath

--- fileInformation/library/fileInformation.h
+++ fileInformation/library/fileInformation.h
@@ -1,40 +1,47 @@
 #include <stdio.h>
-#include<time.h>
+#include <time.h>
+
+#include <sys/stat.h>
+//uidd-> name proprietario
+#include <pwd.h>
+
+#define MAX_SIZE_FILEPATH 200
+
 /**
  * @brief determina la dimensione del file in byte
  * 
  * @param file percorso del file
  * @return dimensione del file
  */
-int dimensioneFile(char* file);
+int dimensioneFile(char *file);
 /**
  * @brief detremina il numero di righe
  * 
  * @param file percorso del file
  * @return il numero di righe, se il file è vuoto ritorna 0
  */
-int numeroRighe(char* file);
+int numeroRighe(char *file);
 /**
  * @brief determina il nome del proprietario
  * 
- * @param file percorso del file
+ * @param pathFile percorso del file
  * @return char* nome del proprietario
  */
-char* nomeProprietario(char* file);
+char *nomeProprietario(char *pathFile);
 /**
  * @brief determina i link presenti nel file
  * 
  * @param file percorso del file
  * @return numero dei link al file.
  */
-int numeroLinkFile(char* file);
+int numeroLinkFile(char *file);
 /**
  * @brief permette di detreminare il numero dei caratteri del file
  * 
- * @param file percorso del file
+ * @param pathFile percorso del file
  * @return numero dei caratteri
  */
-int numeroCaratteri(char* file);
+int numeroCaratteri(char *pathFile);
 /**
  * @brief retituisce i permessi del file per il determinato ruolo
  * 
@@ -42,7 +49,7 @@ int numeroCaratteri(char* file);
  * @param ruolo puo essere admin, group o user
  * @return restituisce l'elenco dei permessi del file per il ruolo indicato.
  */
-char* permessi(char *file, char ruolo);
+char *permessi(char *file, char ruolo);
 /**
  * @brief determina l'ultima modifica fatta
  * 
@@ -50,3 +57,10 @@ char* permessi(char *file, char ruolo);
  * @return la data di ultima modifica
  */
 time_t dataUltimaModifica(char *file);
+/**
+ * @brief determina se il percorso è corretto
+ * 
+ * @param pathFile percorso del file
+ * @return 1 se il percorso è collegato ad un file altrimenti 0 se il percorso non è collegato al file.
+ */
+int checkPath(char *pathFile);
--- fileInformation/src/main.c
+++ fileInformation/src/main.c
@@ -1,6 +1,77 @@
-#include <stdio.h>
+#include "fileInformation.h"
+
 int main(int argc, char const *argv[])
 {
-    printf("hello world \n");
+    char fp[MAX_SIZE_FILEPATH];
+
+    char *pathFile = fp;
+    printf("inserire path file: ");
+    scanf(" %s", pathFile);
+    if (checkPath(pathFile) == 1)
+    {
+        printf("pathfile: %s \n", pathFile);
+        printf("numero caratteri: %d \n", numeroCaratteri(pathFile));
+        printf("nome prorpietario: %s \n", nomeProprietario(pathFile));
+        time_t ddd = dataUltimaModifica(pathFile);
+        struct tm *dataCreazione;
+        dataCreazione = localtime(&ddd);
+        /*
+        - aggiungo 1 al mese perchè il conteggio parte da 0, ovvero gennaio=0:
+        - aggiungo 1900 al anno perchè nella documntazione sottrae 1900;
+        -
+         */
+        printf("data ultima modifica:%d/%d/%d %d:%d \n", dataCreazione->tm_mday, dataCreazione->tm_mon + 1, dataCreazione->tm_year + 1900, dataCreazione->tm_hour + 1, dataCreazione->tm_min);
+    }
     return 0;
+}
+
+int numeroCaratteri(char *pathFile)
+{
+    FILE *fp;
+    int numeChar = 0;
+    if (fp = fopen(pathFile, "r"))
+    {
+        while (!feof(fp))
+        {
+            getc(fp);
+            numeChar = numeChar + 1;
+        }
+    }
+    else
+        printf("Errore file");
+    return numeChar;
+}
+
+char *nomeProprietario(char *pathFile)
+{
+    struct stat buf;
+    struct passwd *pwd;
+    //ottengo informazioni file
+    stat(pathFile, &buf);
+    //convero l'id in nome utente
+    pwd = getpwuid(buf.st_uid);
+
+    return pwd->pw_name;
+}
+time_t dataUltimaModifica(char *pathFile)
+{
+    struct stat buf;
+    stat(pathFile, &buf);
+    struct tm dataCreazione;
+    //ottengo data ultima modifica del file
+
+    dataCreazione = *(gmtime(&buf.st_mtim));
+    //trasformo type tm in type time_t
+    return mktime(&dataCreazione);
+}
+int checkPath(char *pathfFile)
+{
+    FILE *fp;
+    if (fp = fopen(pathfFile, "r"))
+    {
+        fclose(fp);
+        return 1;
+    }
+    else
+        return 0;
 }
